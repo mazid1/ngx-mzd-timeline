77e33217f93c73d8926efeace9976b1ebefc6947
mazid <mazidmailbox@gmail.com>
2020-05-14 19:33:32 +0600

    Timeline component takes TimelineItem as children

--- projects/timeline/src/lib/timeline-item/timeline-item.component.ts
+++ projects/timeline/src/lib/timeline-item/timeline-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: '[mzdTimelineItem]',
@@ -8,10 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TimelineItemComponent implements OnInit {
 
   @Input() border = true;
+  @Input() index = 0;
+  @HostBinding('class.mzd-timeline-container') container: boolean = true;
+  @HostBinding('class.left') left: boolean = true;
+  @HostBinding('class.right') right: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.left = (this.index % 2 === 0);
+    this.right = !this.left;
   }
 
 }
