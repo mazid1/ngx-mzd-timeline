3b4ae20fcf31de919d566fb6300d41da162302f1
Oliveroing <oliveroing@gmail.com>
2021-06-01 16:05:44 -0300

    Dist folder deleted

--- dist/ngx-mzd-timeline/lib/components/icon/icon.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class MzdIconComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- dist/ngx-mzd-timeline/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/timeline.module';
-export * from './lib/components/timeline/timeline.component';
-export * from './lib/components/timeline-content/timeline-content.component';
-export * from './lib/components/icon/icon.component';
